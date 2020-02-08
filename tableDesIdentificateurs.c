22e26792dfc75ae4209a7c17dacf7c6c1b8ddc53
matthieulele <matthieu.edelmann@gmail.com>
2020-02-08 18:59:27 +0100

    modif tableDesIdentificateurs utilisation de liste lineaire triée

--- tableDesIdentificateurs.c
+++ tableDesIdentificateurs.c
@@ -4,30 +4,57 @@
 
 #include "tableDesIdentificateurs.h"
 
-
 int CHERCHER(char nom[LONG_MAX_IDENT]){
-    int indice;
-
-    return indice;
+    int cmpResult;
+    bool trouve = false;
+    int debut = 0;
+    int fin = NB_SYMBOLE - 1;
+    int mil;
+    while (!trouve && debut <= fin){
+        mil = fin+debut/2;
+        cmpResult = strcmp(nom, TABLE_SYMBOLES[TABLE_INDEX[mil]].nom);
+        if(cmpResult == 0) trouve = true;
+        else if (cmpResult == 1) debut = mil + 1;
+        else fin = mil - 1;
+    }
+    if (trouve) return mil;
+    else return -1;
 }
 
-int INSERER(char nom[LONG_MAX_IDENT], TYPE type){
-    int indice;
+int INSERER(char nom[LONG_MAX_IDENT], T_IDENT tIdent){
+    int i = 0;
+    int cmpResult;
+    NB_SYMBOLE++;
+    T_ENREG_IDENT newIdent;
+    strcpy(newIdent.nom, nom);
+    newIdent.tIdent = tIdent;
+    while (cmpResult != 1){ //a finir
+        cmpResult = strcmp(nom, TABLE_SYMBOLES[TABLE_INDEX[i]].nom);
+        i++;
+    }
 
-    return indice;
+
+    TABLE_SYMBOLES[NB_SYMBOLE - 1] = newIdent;
+    return NB_SYMBOLE - 1;
 }
 
 void AFFICHE_TABLE_IDENT(){
-
+    printf("Affichage de la table d'indentificateurs =\n");
+    for (int i = 0; i < NB_SYMBOLE ; ++i) {
+        printf("    %s\n",TABLE_SYMBOLES[TABLE_INDEX[i]]);
+    }
 }
 
 void INITIALISER(){
-    hashTable = malloc(sizeof(struct HashTable));
+    int TABLE_INDEX[NB_IDENT_MAX];
+    NB_SYMBOLE = 0;
+    TABLE_SYMBOLES = malloc(NB_SYMBOLE * sizeof(T_ENREG_IDENT));
+
+   /* hashTable = malloc(sizeof(struct HashTable));
     hashTable->taille = 0;
-    hashTable->contenu = calloc((size_t)hashTable->taille, sizeof(desc_identif));
+    hashTable->contenu = calloc((size_t)hashTable->taille, sizeof(desc_identif));*/
 }
 
 void TERMINER(){
-    free(hashTable->contenu);
-    free(hashTable);
+    free(TABLE_SYMBOLES);
 }
--- tableDesIdentificateurs.h
+++ tableDesIdentificateurs.h
@@ -10,7 +10,7 @@
 #include <string.h>
 #define LONG_MAX_IDENT 20
 
-
+/*
 typedef enum {variable, constante, tableau, parametre, type, procedure, fonction} TYPE;
 
 // Enregistrement décrivant une entrée de la table des symboles
@@ -23,18 +23,25 @@ typedef struct {
     //            un  tableau            alors le contenu est considere comme une dimension
     //            un  parametre         alors le contenu est considere comme un mode de passage
     //            une procedure/fonction alors le contenu est considere comme un nombre de parametre
-    TYPE typeDeSortie //le type de sortie est present lorsque le type est une fonction
-} desc_identif;
+    //TYPE typeDeSortie //le type de sortie est present lorsque le type est une fonction
+} desc_identif;*/
 
-struct HashTable{
-    unsigned int taille;
-    desc_identif** contenu;
-} ;
+typedef enum {variable, constante} T_IDENT;
 
-struct HashTable* hashTable;
+typedef struct {
+    char nom[LONG_MAX_IDENT];  // Nom de l'identificateur*
+    T_IDENT tIdent;
+    int contenu;
+    //si type est une variable           alors le contenu est considere comme une addresse
+    //            une constante          alors le contenu est considere comme une valeur
+} T_ENREG_IDENT;
+
+
 const NB_IDENT_MAX = 100;
-type P_T_ENREG_IDENT = ^T_ENREG_IDENT;
-var TABLE_IDENT: array[][]
+int TABLE_INDEX[NB_IDENT_MAX];
+T_ENREG_IDENT* TABLE_SYMBOLES;
+int NB_SYMBOLE;
+//type P_T_ENREG_IDENT = ^T_ENREG_IDENT;
 
 int CHERCHER(char nom[LONG_MAX_IDENT]);
 int INSERER(char nom[LONG_MAX_IDENT], TYPE type);
