f7998b9f0a742ad2b44e28050925a6d37c713446
valentinflegon <43956710+valentinflegon@users.noreply.github.com>
2020-02-12 22:24:31 +0100

    Create analyseurSyntaxique.h

---
+++ analyseurSyntaxique.h
@@ -0,0 +1,42 @@
+
+#ifndef UNTITLED_ANALYSEURSYNTAXIQUE_H
+#define UNTITLED_ANALYSEURSYNTAXIQUE_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+T_UNILEX UNILEX;
+void ANASYNT();
+
+bool PROG();
+bool DECL_CONST();
+bool DECL_VAR();
+bool BLOC();
+bool INSTRUCTION();
+bool AFFECTATION();
+bool LECTURE();
+bool ECRITURE();
+bool ECR_EXP();
+bool EXP();
+bool SUITE_TERME();
+bool TERME();
+bool OP_BIN();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
