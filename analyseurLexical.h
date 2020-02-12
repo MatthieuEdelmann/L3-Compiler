b91a2067f10f59ac2b7b48f4613a6946d27fb83e
valentinflegon <43956710+valentinflegon@users.noreply.github.com>
2020-02-12 22:21:07 +0100

    Update analyseurLexical.h

--- analyseurLexical.h
+++ analyseurLexical.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 #define LONG_MAX_IDENT 20
 #define LONG_MAX_CHAINE 50
 #define NB_MOTS_RESERVES 7
@@ -15,7 +16,7 @@
 
 typedef enum {motcle, ident, ent, ch, virg, ptvirg, point, deuxpts, parouv, parfer, inf, sup, eg, plus, moins, mult, divi, infe, supe, diff, aff} T_UNILEX;
 
-FILE *SOURCE;
+FILE *SOURCE ;
 char CARLU;
 int NOMBRE;
 char CHAINE[LONG_MAX_CHAINE];
@@ -26,15 +27,14 @@ int CONST=0;
 void ERREUR(int numeroErreur);
 void LIRE_CAR();
 void SAUTER_SEPARATEURS();
+
 T_UNILEX RECO_ENTIER();
 T_UNILEX RECO_CHAINE();
 T_UNILEX RECO_IDENT_OU_MOT_RESERVE();
-bool EST_UN_MOT_RESERVE();
 T_UNILEX RECO_SYMB();
-void ANALEX();
+T_UNILEX ANALEX();
 void INSERE_TABLE_RESERVES(char nouveauMot[]);
+bool EST_UN_MOT_RESERVE();
 void INITIALISER();
 void TERMINER();
-
-
 #endif //UNTITLED_ANALYSEURLEXICAL_H
