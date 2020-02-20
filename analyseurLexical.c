8b88c2b210a2dab3688fb758925e0c0819718079
valentinflegon <43956710+valentinflegon@users.noreply.github.com>
2020-02-20 02:28:57 +0100

    Update analyseurLexical.c

--- analyseurLexical.c
+++ analyseurLexical.c
@@ -1,254 +1,259 @@
-#include "analyseurSyntaxique.h"
 #include "analyseurLexical.h"
 
-
-void ANASYNT(){
-    UNILEX = ANALEX();
-    if(PROG()){
-        printf("Le programme source est syntaxiquement correct\n");
+void ERREUR(int numeroErreur){
+    switch (numeroErreur){
+          case 0:
+            printf("Erreur n°%d / Ligne n°%d :ERREUR syntaxique",numeroErreur, NUM_LIGNE);
+            break;
+        case 1:
+            printf("Erreur n°%d / Ligne n°%d : Fin de fichier atteinte ",numeroErreur, NUM_LIGNE);
+            break;
+        case 2:
+            printf("Erreur n°%d / Ligne n°%d : L'entier lu depasse la taille maximale",numeroErreur, NUM_LIGNE);
+            break;
+        case 3:
+            printf("Erreur n°%d / Ligne n°%d : La chaine de characteres depasse la longeur maximale",numeroErreur, NUM_LIGNE);
+            break;
+        case 4:
+            printf("Erreur n°%d / Ligne n°%d : Caractère non valide ",numeroErreur, NUM_LIGNE);
+            break;
+        case 5:
+            printf("Erreur n°%d / Ligne n°%d : Mauvaise initialisation du tableau mot réervé ",numeroErreur, NUM_LIGNE);
+            break;
     }
-    else ERREUR(0); //erreur syntaxique
-}
+    exit(0);
+}// TEST bon 
 
-bool PROG(){   
-    printf(" PROG \n");
-    // PRGRAMME ident ;
-    //DECL_CONST 
-    //DECL_VAR
-    // BLOC
-    if ((UNILEX == motcle ) && (strcmp(CHAINE, "PROGRAMME") == 0)){// PROGRAMME 
-        printf(" PROGRAMME ");
-        UNILEX = ANALEX();
-        if (UNILEX  == ident){ //IDENT 
-            printf(" IDENT ");
-            UNILEX = ANALEX();
-            if (UNILEX == ptvirg){  // point virgule  
-                printf(" ; ");
-                LIRE_CAR();// \n
-                //DECL_CONST 
-                //DECL_VAR
-                if(BLOC()){
-                   // UNILEX = ANALEX();
-                    //if (UNILEX == point){  // point
-                      //  printf(" . \n");
-                        return true;
-                   // }
-                }
-                else return false;
-            }
-            else return false;
-        }
-        else return false;
-    }
-    else return false;
-}
-
-bool DECL_CONST(){
-    return true;
-}
-
-bool DECL_VAR(){
-    return true;
-}
-
-bool BLOC(){
-    printf("BLOC \n");
-    //DEBUT 
-    //INSTRUCTION
-    //...
-    //FIN
-    bool fin = false,erreur;
-    UNILEX = ANALEX();
-    if ((UNILEX == motcle) && (strcmp(CHAINE, "DEBUT") == 0)){//  DEBUT  
-        printf(" DEBUT \n");
-        UNILEX = ANALEX();//
-        if (INSTRUCTION()){// INSTRUCTION
-            UNILEX = ANALEX();
-            while (fin == false){    
-                if (UNILEX == ptvirg){
-                    printf(" ; \n");
-                    LIRE_CAR();
-                    UNILEX = ANALEX();
-                    INSTRUCTION();
-                    UNILEX = ANALEX();
-                }
-                else {  
-                    LIRE_CAR();
-                    printf(" \n");
-                    fin=true;
-                }
-            }
-            UNILEX = ANALEX();        
-            if ((UNILEX == motcle) && (strcmp(CHAINE, "FIN") == 0) ){ // FIN  
-                printf(" FIN  "); 
-                return true;
-            }
-            else return false;
+void LIRE_CAR(){
+    if (SOURCE != NULL){
+        CARLU = fgetc(SOURCE);
+        if (CARLU == '\n'){
+            NUM_LIGNE++ ; //Fin de ligne
+            LIRE_CAR();
         } 
-        else return false;
-    }
-    else return false;
-}
-
-bool INSTRUCTION(){
-    // AFFECTATION | LECTURE | ECRITURE | BLOC 
-    printf (" INSTRUCTION  -->");
-    if ( AFFECTATION() || ECRITURE() || LECTURE() || BLOC() ) {     
-        return true; 
-    }
-    else return false;
-}
-
-bool AFFECTATION(){   
-    // ident := x_ 
-    if (UNILEX == ident){ 
-        printf(" AFFECTATION --> ");
-        printf(" IDENT -> ");
-        UNILEX = ANALEX(); 
-        if (UNILEX == aff){  
-            printf("%s := ",CHAINE);
-            UNILEX = ANALEX();
-            if(EXP()){             
-                return true;
-            }
-            else return false;  
+        if (CARLU == EOF) ERREUR(1); //Fin de fichier
         }
-        else return false; // erreur syntaxique dnas une instruction d'affectation 
-    }
-    else return false; // erreur syntaxique dans une instruction d'affectation 
-}
+}// TEST bon
 
-bool LECTURE(){
-    // bug il faut ecrire 
-    // LIRE (a )_ 
-    // LECTURE (x , y ,z)
-    bool fin,erreur;
-    if ((UNILEX  == motcle) && (strcmp(CHAINE, "LIRE") == 0)){ //LIRE  add && LIRE 
-        printf(" LECTURE --> ");
-        printf("LIRE ");
-        UNILEX = ANALEX();
-        if (UNILEX == parouv){     
-            printf(" ( ");
-            UNILEX = ANALEX();
-            if (UNILEX == ident){
-                printf (" IDENT -> ");
-                printf("%s",CHAINE);
-                fin = false;
-                erreur = false;
-                while (fin == false){
-                    UNILEX = ANALEX();
-                    if (UNILEX == virg){
-                        printf(" , ");
-                        UNILEX = ANALEX();
-                        if (UNILEX == ident){
-                          printf("%s",CHAINE);
-                        }
-                        else {
-                            printf("erreur pas ident");
-                            fin = true;
-                            erreur = true ;
-                        }
-                    }
-                    else fin = true;
-                }
-                if (UNILEX == parfer){
-                    printf(" ) ");
-                    return true; 
-                }
-                else return false;//erreur syntaxique dans instruction de lecture: ')' attendu 
-            }
-            else return false;//erreur syntaxique dans instruction de lecture: 'ident' attendu 
+void SAUTER_SEPARATEURS() {
+    bool flag = false;
+    while (!flag) {
+        LIRE_CAR();//
+        while (CARLU == ' '  || CARLU == '\n') {
+            LIRE_CAR();
         }
-        else return false;//erreur syntaxique dans instruction de lecture: '(' attendu 
-   }
-    else return false;//erreur syntaxique dans instruction de lecture: mot clé 'LIRE' attendu
-}
-
-bool ECRITURE(){
-    
-    //ECRIRE (a )_
-    //ECRIRE ('a')_
-    //ECRIRE ( )_
-    // ecrire plusieurs
-    bool fin,erreur;
-    if ((UNILEX  == motcle ) && (strcmp(CHAINE, "ECRIRE") == 0)){ //ECRIRE  add && ECRIRE 
-        printf(" ECRITURE --> ");
-        printf(" ECRIRE ");
-        UNILEX = ANALEX();
-        if (UNILEX == parouv){
-            printf(" ( ");
-            UNILEX = ANALEX();
-            erreur = false;
-            if (ECR_EXP()){ 
-                printf("%s",CHAINE);
-              /*  UNILEX = ANALEX();
-                fin = false;
-               do{
-                    if (UNILEX == virg){
-                       UNILEX = ANALEX();// a 
-                        
-                        erreur != ECR_EXP();
-                        if (erreur){
-                            fin = true;
-                        }
-                        
-                         UNILEX = ANALEX(); // ) 
-                         
-                    } 
-                    else fin = true;
-              //  }while (fin == false);*/
+        if (CARLU == '{') {
+            while (CARLU != '}') {
+                LIRE_CAR();
             }
-          //  if (erreur){return false;} //erreur syntaxique dans instruction d'ecriture: expression incorrecte
-            UNILEX = ANALEX();
-            if (UNILEX == parfer){
-                 printf(" ) ");
-                return true;
-            }
-            else return false; //erreur syntaxique dnas instruction d'ecriture: ')' attendu 
+            LIRE_CAR();
         }
-        else return false; //erreur syntaxique dans instruction d'ecriture: '(' attendu 
+        flag = true;
     }
-    else return false; //erreur syntaxique dans instruction d'ecriture: mot clé 'ECRIRE' attendu 
-}
+}// TEST bon
 
-bool ECR_EXP(){
-    // EXP | 'ch'
-    if ( UNILEX == ch ){
-        //UNILEX = ANALEX();
-        return true;
+T_UNILEX RECO_ENTIER(){ 
+    char* CHAINE= malloc(sizeof(CHAINE));
+    while (((int)CARLU >= 48) && ((int)CARLU <= 57)) { 
+       CHAINE = strcat(CHAINE,&CARLU);
+        LIRE_CAR();
+    }
+    NOMBRE = atoi(CHAINE);
+    if(NOMBRE>MAX_INT)  ERREUR(2); 
+    return ent;
+}// TEST bon 
+
+T_UNILEX RECO_CHAINE(){
+     for (int j=0;j < LONG_MAX_CHAINE;j++){
+          CHAINE[j] = NULL;
+    }
+    int i =0;
+    LIRE_CAR();
+    while ((int)CARLU != 39){
+        if (i  >= LONG_MAX_CHAINE) ERREUR(3);
+        CHAINE[i] = CARLU; 
+        LIRE_CAR();
+        i++;
+    }
+    LIRE_CAR();
+    return ch;
+}//TEST bon 
+
+T_UNILEX RECO_IDENT_OU_MOT_RESERVE(){
+     int j=0;
+     for (j=0;j < LONG_MAX_CHAINE;j++){
+          CHAINE[j] = NULL;
+    }
+    int i = 0;
+    while(((int)CARLU >= 48 && (int)CARLU <= 57) || ((int)CARLU >= 65 && (int)CARLU <= 90) || ((int)CARLU >= 97 && (int)CARLU <= 122) || CARLU == '_'){
+        if (i < LONG_MAX_CHAINE) {
+            if ((int)CARLU >= 97 && (int)CARLU <= 122) CARLU -= 32;{
+                toupper(CARLU);
+                CHAINE[i] = CARLU;
+               // printf("%c",CARLU);
+            }
+            LIRE_CAR(); 
+            i++;
+        }
+    }
+
+    if (EST_UN_MOT_RESERVE()){
+        LIRE_CAR(); 
+      //  printf(" mot clé ");
+        return motcle;
     } 
-    //else return false;
-}
-   
-bool EXP(){
-    printf(" EXP \n");
-     UNILEX = ANALEX();
-    // TERME SUITE_TERME 
-    if (TERME() && SUITE_TERME() ){
-        return true;
+    else {
+        LIRE_CAR();
+        return ident;
     }
-    else return false;
-}
+}// TEST bon 
 
-bool SUITE_TERME(){
-    //  '  ' | OP_BIN EXP
-  //  if (TERME() && OP_BIN()){
-        return true;
-   // }
-   // else return false;
-}
+bool EST_UN_MOT_RESERVE(){
+    int j,i,w;
+    int sizeChaine = strlen(CHAINE);  
+    for (int k=0;k<NB_MOTS_RESERVES;k++){
+        w=0;
+        for (i = 0; i<sizeChaine;i++){
+            if (CHAINE[i] == TABLE_MOTS_RESERVES[k][i]){
+                w++;
+            }    
+        } 
+        if (w == sizeChaine ){
+            return true;
+        }
+    }   
+    return false;       
+}// a amélioré 
 
-bool TERME(){
-    // ENT | IDENT | ( EXP ) | - TERME 
-  //  if (UNILEX == ent || UNILEX == ident ){
-        return true; 
-   // }
-    //else return false;
-}
+T_UNILEX RECO_SYMB(){
+    switch (CARLU)
+    {
+    case ',':
+        LIRE_CAR();
+        return virg;
+        break;
+    case ';':
+        LIRE_CAR();
+        return ptvirg;
+        break;
+    case '.':
+        LIRE_CAR();
+        return point;
+        break;
+    case '=':
+        LIRE_CAR();
+        return eg;
+        break;
+    case '+':
+        LIRE_CAR();
+        return plus;
+        break;
+    case '-':
+        LIRE_CAR();
+        return moins;
+        break;
+    case '*':
+        LIRE_CAR();
+        return mult;
+        break;
+    case '/':
+        LIRE_CAR();
+        return divi;
+        break;
+    case '(':
+        LIRE_CAR();
+        return parouv;
+        break;
+    case ')':
+        LIRE_CAR();
+        return parfer;
+        break;
+    case '>':
+        LIRE_CAR();
+        if(CARLU == '=' )
+            LIRE_CAR();
+            return supe;
+            break;
+        if(CARLU != '='){
+            LIRE_CAR();
+            return sup;
+            break;
+        }
+    case ':':
+        LIRE_CAR();
+        if(CARLU == '=' )
+            LIRE_CAR();
+            return aff;
+            break;
+        if (CARLU != '='){
+            return deuxpts;
+            break;
+        }
+    case '<':
+        LIRE_CAR();
+        if(CARLU == '=' )
+            LIRE_CAR();
+            return infe;
+            break;
+        if( CARLU == '>'){ 
+            LIRE_CAR();
+            return diff;
+            break;
+        }
+        else
+        {
+            return inf;
+            break;
+        }
+    default:
+        ERREUR(4); // si le caractere n'est pas compris parmi eux alors on le connais
+        break;
+    }
+}// TEST bon
 
-bool OP_BIN(){
-    //if (UNILEX == plus || UNILEX == moins || UNILEX == mult || UNILEX == div){
-        return true;
-  //  }
-  //  else return false;
-}
+T_UNILEX ANALEX(){
+    if ((CARLU == ' ')|| (CARLU == '{')){
+        SAUTER_SEPARATEURS();
+    }
+    else if (((int)CARLU >= 48) && ((int)CARLU <= 57)){ // un entier
+        RECO_ENTIER();
+    }
+    else if ((int)CARLU == 39){ // une apostrphe
+        RECO_CHAINE();
+    }
+    else if (((int)CARLU >= 65 && (int)CARLU <= 90) || ((int)CARLU >= 97 && (int)CARLU <= 122)){     //reco ident OU MOT CLE
+        RECO_IDENT_OU_MOT_RESERVE();
+    }
+    else
+
+        RECO_SYMB();
+}// TEST bon 
+
+void INSERE_TABLE_RESERVES(char nouveauMot[]){
+   int x=  strlen(nouveauMot);
+   if (x <= 9 && CONST <= NB_MOTS_RESERVES) {
+        for (int i =0; i<x;i++){
+            TABLE_MOTS_RESERVES[CONST][i] = nouveauMot[i];
+        }
+    }
+    else ERREUR(5);
+    CONST ++;
+} // TEST bon 
+
+void INITIALISER(){
+    NUM_LIGNE = 1;
+    SOURCE = fopen("SOURCE.txt", "r");
+    LIRE_CAR();// pour commencer 
+    INSERE_TABLE_RESERVES("PROGRAMME");
+    INSERE_TABLE_RESERVES("DEBUT");
+    INSERE_TABLE_RESERVES("FIN");
+    INSERE_TABLE_RESERVES("CONST");
+    INSERE_TABLE_RESERVES("VAR");
+    INSERE_TABLE_RESERVES("ECRIRE");
+    INSERE_TABLE_RESERVES("LIRE");
+    
+}//TEST bon
+
+void TERMINER(){
+    fclose(SOURCE);
+}// TEST bon
